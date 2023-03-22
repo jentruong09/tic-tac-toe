115425ee436f9b40d9b46d3111e0dce675796a3b
Jen Truong <jennifertruong09@gmail.com>
2023-03-22 11:00:08 -0400

    added js for placing x and circle

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const xClass = 'x'
+const circleClass = 'circle'
+const cellElements = document.querySelectorAll('[data-cell]')
+let circleTurn
 
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true }) // last part - can click a specific cell only once
+})
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? circleClass : xClass
+    // placing a mark
+    placeMark(cell, currentClass)
+    // check for win
+
+    // check for draw
+
+    // switching turns
+    swapTurns()
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn
+}
